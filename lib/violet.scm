0d246891bdabfeda3bc49cbebc4b79b55a286bbe
Toru Hisai <toru@torus.jp>
2019-03-20 19:35:24 +0100

    show stack trace

--- lib/violet.scm
+++ lib/violet.scm
@@ -30,7 +30,7 @@
     (lambda ()
       (let loop ()
         (let ((task (dequeue/wait! *task-queue*)))
-          (guard (exc [else (print exc)])
+          (guard (exc [else (report-error exc)])
                  (task))             ; task may return multiple times!
           (flush))
         (loop))))))
@@ -111,7 +111,7 @@
                 (make-thread
                  (^[]
                    (let ((result 
-                          (guard (exc [else (x->string exc)])
+                          (guard (exc [else (report-error exc)])
                                  (proc))))
                      (enqueue-task! (^[] (cont result)))))))
                (yield)))))
