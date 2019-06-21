d1651d93fa7def5ed00b3256b680f71ea37cb427
Toru Hisai <toru@torus.jp>
2019-06-21 22:50:39 +0200

    rework polynomial and distance

--- lib/trader.scm
+++ lib/trader.scm
@@ -47,15 +47,21 @@
 (define (high-of row) (cadddr row))
 
 (define (make-line-poly x0 y0 x1 y1)
-  ;; returns polynomial in x + by + c = 0
-  (let* ((b (- (/ (- x0 x1) (- y0 y1))))
-         (c (- (+ x0 (* b y0)))))
-    (cons b c)))
+  ;; returns polynomial in ax + by + c = 0
+  (if (= y0 y1)
+      (list 0 1 (- y0))
+      (let* ((b (- (/ (- x0 x1) (- y0 y1))))
+             (c (- (+ x0 (* b y0)))))
+        (when (or (nan? b) (nan? c) (infinite? b) (infinite? c))
+          #?=(list b c x0 y0 x1 y1)
+          (error "invalid!"))
+        (list 1 b c))))
 
 (define (distance-to-line x y poly)
-  (let ((b (car poly))
-        (c (cdr poly)))
-    (+ y (/ (+ x c) b))))
+  (let ((a (car poly))
+        (b (cadr poly))
+        (c (caddr poly)))
+    (- y (- (/ (+ (* a x) c) b)))))
 
 (define (line-from-rows rows pick filter accum step)
   (define (kdr lst)
@@ -84,15 +90,18 @@
                               (total-distance 0)
                               (count 0))
                     (if (null? rows)
+                        (begin
+                          (list x0 x1 total-distance min-total-distance)
                         (if (> min-total-distance total-distance)
                             (loop2 (kdr rest) line-poly     total-distance     (+ x1 step))
                             (loop2 (kdr rest) min-line-poly min-total-distance (+ x1 step)))
+                        )
                         (let* ((distance (distance-to-line count (pick (car rows)) line-poly)))
                           (if (or (= count x0) (= count x1))
                               (loop3 (kdr rows) total-distance (+ count step))
                               (if (filter distance)
-                                  (loop2 (kdr rest) min-line-poly min-total-distance (+ x1 step))
-                                  (loop3 (kdr rows) (accum total-distance distance) (+ count step)))
+                                  (loop3 (kdr rows) (accum total-distance distance) (+ count step))
+                                  (loop2 (kdr rest) min-line-poly min-total-distance (+ x1 step)))
                               )))))))))))
 
 (define (splice-data data offset length)
@@ -103,17 +112,35 @@
 (define rows-of caddr)
 (define count-of cadddr)
 
-(define (min-line/range data offset points)
-  (offset-line (line-from-rows (splice-data data offset points) low-of negative? + 1) offset))
+;; (define (min-line/range data offset points)
+;;   (offset-line (line-from-rows (splice-data data offset points) low-of negative? - 1) offset))
 
-(define (max-line/range data offset points)
-  (offset-line (line-from-rows (splice-data data offset points) high-of positive? - 1) offset))
+;; (define (max-line/range data offset points)
+;;   (offset-line (line-from-rows (splice-data data offset points) high-of negative? - 1) offset))
+
+(define (square-add a b)
+  (let ((dest (+ a (* b b))))
+    (when (nan? dest)
+      #?=(cons a b)
+      (error "NAN!"))
+    dest))
 
 (define (min-line/range/step data offset points step)
-  (offset-line (line-from-rows (splice-data data offset points) low-of negative? + step) offset))
+  (offset-line (line-from-rows (splice-data data offset points) low-of positive? square-add step) offset))
 
 (define (max-line/range/step data offset points step)
-  (offset-line (line-from-rows (splice-data data offset points) high-of positive? - step) offset))
+  (offset-line (line-from-rows (splice-data data offset points) high-of negative? square-add step) offset))
+
+(define (min-line/range data offset points)
+  (min-line/range/step data offset points 1))
+
+(define (max-line/range data offset points)
+  (max-line/range/step data offset points 1))
 
 (define (offset-line poly offset-x)
-  (cons (car poly) (- (cdr poly) offset-x)))
+  (let ((a (car poly))
+        (b (cadr poly))
+        (c (caddr poly)))
+    (if (zero? a)
+        poly
+        (list a b (- c offset-x)))))
