ef7b3ffbcb23ea3c90de528ece3369577de19093
Toru Hisai <toru@torus.jp>
2019-10-07 11:38:09 +0200

    restart x11vnc when crash

--- tws/starttws.sh
+++ tws/starttws.sh
@@ -1,3 +1,3 @@
 /usr/bin/Xvfb :0 -ac -screen 0 1024x768x24 &
-sleep 5 && /usr/bin/x11vnc -nonc -viewpasswd remote_view_only_pass -passwd some_pass123 -display :0 -forever -shared -noipv6 -localhost &
+(while true; do sleep 5; /usr/bin/x11vnc -nonc -viewpasswd remote_view_only_pass -passwd some_pass123 -display :0 -forever -shared -noipv6; done) &
 sleep 10 && ~/Jts/978/tws
