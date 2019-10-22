e6344a60096379f1802b36cd6ad589ff001c21de
Toru Hisai <toru@torus.jp>
2019-10-22 10:06:16 +0200

    scan more currency pairs.

--- scan.scm
+++ scan.scm
@@ -1,9 +1,11 @@
+(use util.match)
+(use srfi-19)
+
+(use redis)
+
 (add-load-path "./lib/")
 (use scanner)
 (use config)
-(use redis)
-(use srfi-19)
-
 (use trader)
 
 (define *conn* (redis-open redis-host 6379))
@@ -13,11 +15,9 @@
 
 ;; Function: make-date nanosecond second minute hour day month year zone-offset
 
-(define *eur-gbp* (make-currency-pair "EUR" "GBP"))
-
-(define *eur-gbp-15min*
+(define (make-style sym cur)
   (make-trading-style
-   *eur-gbp*
+   (make-currency-pair sym cur)
    "IDEALPRO"
    "15 mins"
    "900 S"
@@ -26,43 +26,40 @@
    "1 W"
    ))
 
-(define *eur-gbp-1hour*
-  (make-trading-style
-   *eur-gbp*
-   "IDEALPRO"
-   "1 hour"
-   "3600 S"
-   "3660 S"
-   "1 Y"
-   "4 W"
-   ))
-
 (define *total-gain* 0)
+(define *total-orders* 0)
+
 (define (close-positiion pos)
   (let ((gain (cadr (cdddr pos))))
     (set! *total-gain* (+ *total-gain* gain))
-    (print `(close ,*total-gain* ,pos))))
+    (inc! *total-orders*)
+    (print `(close ,*total-orders* ,*total-gain* ,pos))))
 
-(define (main . args)
-  (let* ((d1 (make-date 0 0 15 0 2 7 2019 0))
+(define (main args)
+  (match args ((com sym cur) (scan-main sym cur))))
+
+(define (scan-main sym cur)
+  (let* ((d1 (make-date 0 0 0 0 14 10 2019 0))
          ;(d1 (make-date 0 0 0 20 23 8 2019 0))
          (t1 (date->time-utc d1))
          ;(d2 (make-date 0 0 0 2 24 8 2019 0))
-         (d2 (make-date 0 0 15 0 20 9 2019 0))
-         (t2 (date->time-utc d2)))
+         (d2 (make-date 0 0 0 0 18 10 2019 0))
+         (t2 (date->time-utc d2))
+         (style (make-style #?=sym #?=cur)))
     (let loop ((index 0)
                (t t1)
                (positions ()))
       (if (time<? t t2)
           (let ((date (time-utc->date t)))
             (let-values (((pos poss)
-                          (inspect *conn* *eur-gbp-15min* date positions index close-positiion)))
+                          (inspect *conn* style date positions index close-positiion)))
               (if (and pos
-                       (> (pos-info-gain (position-info pos)) 0.0001)) ; > 1 pips
+                       (> (pos-info-gain (position-info pos)) 0.0010)) ; > 10 pips
                   (begin
+                    (inc! *total-orders*)
                     (write (serialize-position pos))(newline)
                     (loop (+ index 1) (add-duration t fifteen-mins) (cons pos poss)))
                   (loop (+ index 1) (add-duration t fifteen-mins) poss))
               ))
           (print (cons 'positions (map position-index positions)))))
-    (print `(total ,*total-gain*))))
+    (print `(total ,*total-orders* ,*total-gain*))))
