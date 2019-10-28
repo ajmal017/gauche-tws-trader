9486a51f89479baaefd77ec54dfa5122dcd00898
Toru Hisai <toru@torus.jp>
2019-10-28 15:34:42 +0100

    some more scanning

--- sum.scm
+++ sum.scm
@@ -6,28 +6,29 @@
 
 (define *positions* (make-hash-table))
 
-(define *thresholds* '(0.0001 0.0005 0.001 0.005))
-(define *total-with-threadholds* (make-hash-table))
+(define *gain-thresholds* '(0.01 0.02 0.03 0.04 0.05))
+(define *total-with-thresholds* (make-hash-table))
 
 (let loop ((total-gain 0))
   (let ((line (read)))
     (if (eof-object? line)
         (begin
-          (print (hash-table->alist *total-with-threadholds*)))
+          (display (hash-table->alist *total-with-thresholds*) (current-error-port))
+          (newline (current-error-port)))
         (match line
                (('position _ ...)
                 (let ((pos (deserialize-position line)))
                   (hash-table-put! *positions* (position-index pos) pos)
                   (loop total-gain)))
-               (('close total ('close pos-idx price result gain))
+               (('close id total pos-idx-close ('close pos-idx price result gain))
                 (let* ((pos (hash-table-get *positions* pos-idx))
                        (info (position-info pos)))
                   (for-each (lambda (thr)
-                              (if (> gain thr)
-                                  (hash-table-put! *total-with-threadholds* thr
-                                                   (+ (hash-table-get *total-with-threadholds* thr 0)
+                              (if (< (pos-info-gain info) thr)
+                                  (hash-table-put! *total-with-thresholds* thr
+                                                   (+ (hash-table-get *total-with-thresholds* thr 0)
                                                       gain))))
-                            *thresholds*)
-                  (print #`",(pos-info-gain info) ,gain")
+                            *gain-thresholds*)
+                  (print #`",(pos-info-long-trend-error info) ,gain ,id")
                   (loop (+ total-gain gain))))
                (else (loop total-gain))))))
